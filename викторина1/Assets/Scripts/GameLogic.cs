e41df1b8e4e9dd75ea882bf024eb3f1fa70914f3
Andrey <124148072+AndreyAferov@users.noreply.github.com>
2023-08-20 17:33:20 +0300

    This my second commit! In this commit I made the questions

--- викторина1/Assets/Scripts/GameLogic.cs
+++ викторина1/Assets/Scripts/GameLogic.cs
@@ -1,12 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
+using TMPro;
 using UnityEngine;
 
 public class GameLogic : MonoBehaviour
 {
-
     public QuestionList[] questions;
+    public TextMeshProUGUI qTextTMP; // Компонент TextMeshProUGUI для вывода вопросов
 
+    public void OnClickPlay()
+    {
+        int randomIndex = Random.Range(0, questions.Length);
+        qTextTMP.text = questions[randomIndex].Question;
+    }
 }
 
 [System.Serializable]
